3b21dddb4d79bafb581abbaa57527bb5f5f42f0d
Łukasz Goleniec <30418440+Flashrow@users.noreply.github.com>
2020-11-01 15:57:20 +0100

    nice look

--- src/screens/Calendar/Calendar.js
+++ src/screens/Calendar/Calendar.js
@@ -4,52 +4,66 @@ import React from "react"
 import styled from 'styled-components'
 
 export function Calendar(){
-
-
-
+    const dayNames = ['pon', 'wto', 'śro', 'czw', 'pią', 'sob', 'nie'];
+    const monthNames = ['Styczeń', 'Luty', 'Marzec', 'Kwiecień', 'Maj', 'Czerwiec', 'Lipiec', 'Sierpień', 'Wrzesień', 'Październik', 'Listopad', 'Grudzień'];
     var monthDays = new Array(7*6);
     var now = new Date();
 
     var startingDate = new Date(now.setDate(1));
-    var calendarStartingDate = new Date(startingDate.setDate(startingDate.getDate() - startingDate.getDay() + 1));
-    console.log("StartingNumber: " + calendarStartingDate);
-    
+    var calendarStartingDate = new Date(startingDate.setDate(startingDate.getDate() - ((startingDate.getDay()+7)%8) + 1));
+    //console.log("StartingNumber: " + calendarStartingDate + "\nStarting date: " + startingDate);
+
     const days = [];
-    var i = 0;
-    for(var x = 1; x<=6; x++){
-        const row = [];
-        for(var y = 1; y<=7; y++){
-            monthDays[i] = calendarStartingDate.getDate();
-            calendarStartingDate.setDate(calendarStartingDate.getDate() + 1);
-            row.push(
-            <Grid item xs auto alignContent = 'space-around'>
-               <div style = {{backgroundColor: 'white', height: 100, margin: 1, border: 'black',}}>
-                   <p style = {{textAlign: "center" ,color: "black"}}>
-                   {monthDays[i]}
-                   </p>
-                </div>
-            </Grid>
-            );
-            i++;
+
+    for(var i = 1; i<=6*7; i++){
+        monthDays[i] = calendarStartingDate.getDate();
+        calendarStartingDate.setDate(calendarStartingDate.getDate() + 1);
+        if(i<=7){
+            days.push(
+                <GridItem>
+                <p style = {{textAlign: "center" ,color: "gray", fontSize: 24, fontWeight: 'Medium', marginTop: -30}}>{dayNames[i-1]}</p>
+                <p style = {{textAlign: "center" ,color: "black", fontSize: 18, fontWeight: 'Bold', marginTop: -25}}>
+                 {monthDays[i]}
+                </p>
+                </GridItem>
+                );
+        }else{
+            days.push(
+                <GridItem>
+                <p style = {{textAlign: "center" ,color: "black", fontSize: 18, fontWeight: 'Bold', marginTop: -25}}>
+                 {monthDays[i]}
+                </p>
+                </GridItem>
+                );
         }
-        days.push(
-            <Grid container item 
-            alignItems = 'stretch'
-            alignContent = 'space-around'
-            spacing = {0}
-            >
-                    {row}
-            </Grid>
-        )
-    }
-    
+    }  
 
     return( 
-        <Grid container spacing ={0}
-        alignContent = 'space-around'
-        style = {{backgroundColor:'white'}}>
+        <div>
+        <p style = {{color: "black", fontSize: 24, fontWeight: 'Bold', marginTop: -30}}>
+            {monthNames[now.getMonth()]} {now.getFullYear()}
+        </p>
+        <GridContainer>
             {days}
-        </Grid>
+        </GridContainer>
+        </div>
     )
-
 }
+
+const GridContainer = styled.div`
+display: grid;
+height: 90%;
+grid-auto-rows: 1fr;
+background-color: #9C9083;
+border-radius: 25;
+grid-template-columns: repeat(7, auto);
+padding-right: 1px;
+padding-bottom: 1px;
+`
+
+const GridItem = styled.div`
+padding: 30px;
+background-color: white;
+margin-left: 1px;
+margin-top: 1px;
+`
