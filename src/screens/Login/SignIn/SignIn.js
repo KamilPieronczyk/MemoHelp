b18ed722c4e1e84b54958d8becc79ffbbc3b4dc3
Kamil <kamilpier08@gmail.com>
2021-01-10 14:19:36 +0100

    google icon

--- src/screens/Login/SignIn/SignIn.js
+++ src/screens/Login/SignIn/SignIn.js
@@ -1,45 +1,18 @@
-//import { Container } from '@material-ui/core'
-//import React from 'react'
 import styled from 'styled-components'
-//import Checkbox from '@material-ui/core/Checkbox';
 import React, { useState } from 'react';
-//import { withStyles } from '@material-ui/core/styles';
-//import { green } from '@material-ui/core/colors';
 import FormControlLabel from '@material-ui/core/FormControlLabel';
 import Checkbox from '@material-ui/core/Checkbox';
 import validator from 'validator';
 import firebase from 'firebase';
 import history from '../../../history';
 import {
-    //BrowserRouter as Router,
-    //Switch,
-    //Route,
     Link
   } from "react-router-dom";
 import {Button} from '../../../components'
 import {useSnackbar} from 'notistack'
 
-// const GreenCheckbox = withStyles({
-//     root: {
-//         color: green[400],
-//         '&$checked': {
-//             color: green[600],
-//         },
-//     },
-//     checked: {},
-// })((props) => <Checkbox color="default" {...props} />);
-
 export function LoginForm() {
-    // const [state, setState] = useState({
-    //     checkedA: true,
-    //     checkedB: true,
-    //     checkedF: true,
-    //     checkedG: true,
-    // });
     const [state, setState] = useState(false);
-    // const handleChange = (event) => {
-    //     setState({ ...state, [event.target.name]: event.target.checked });
-    // };
     const handleChange = (event) => {
         setState(!state);
     };
@@ -97,16 +70,6 @@ export function LoginForm() {
                   });
             });
         }
-        // firebase.auth().signInWithEmailAndPassword(mail, password).then(()=>{
-        //     console.log("zalogowano");
-        // }).catch(function(error) {
-        //     // Handle Errors here.
-        //     //var errorCode = error.code;
-        //     var errorMessage = error.message;
-        //     console.log("firebase auth:",errorMessage);
-        //     // ...
-        //   });
-        
     }
     var provider = new firebase.auth.GoogleAuthProvider();
     const gLogin = () => {
@@ -132,12 +95,8 @@ export function LoginForm() {
                 email: email,
                 userName: userName
             }).then(function(){
-                // history.push("/");
-                // window.location.reload();
                 setButtonDisabled(false);
             });
-
-            //setButtonDisabled(false);
           }).catch(function(error) {
             // Handle Errors here.
             var errorCode = error.code;
@@ -151,6 +110,8 @@ export function LoginForm() {
             // ...
           });
     }
+
+    const GoogleIcon = () => <img src="https://img.icons8.com/fluent/48/000000/google-logo.png"/>;
     return (
         <LoginContainer>
             <MyInput type="email" id="fname" name="fname" placeholder="e-mail" onChange={e=>setMail(e.target.value)}>
@@ -182,7 +143,7 @@ export function LoginForm() {
 						width: '48%',
 						color: 'black',
 					}}
-                    text="Zaloguj się g"
+                    text={<span style={{display: 'flex', justifyContent: 'space-between', alignItems: 'center', width: '100%'}}>Zaloguj się {GoogleIcon()}</span>}
                     onClick={gLogin}
                     disabled={buttonDisabled}
 				/>
@@ -242,43 +203,23 @@ const EmptyBox = styled.div`
     //text-align:center;
     color: #73909C;
 `
-// const Zaloguj = styled.div`
-//     //width: 200px;
-//     //height: 50px;
-//     //border: 2px solid #73909C;
-//     background-color: #73909C;
-//     color: #FFFFFF;
-//     text-align: center;
-//     font-size: 16px;
-//     border-radius: 10px;
-//     padding: 16px;
-//     box-shadow: 0px 4px 4px rgba(0, 0, 0, 0.25);
-//     //width:100%;
-// `
 const Google = styled.div`
-    //width: 200px;
     height: 48px;
-    //border: 2px solid #FFFFFF;
     flex-direction:row;
     display:flex;
     background-color: #FFFFFF;
-    //color: #FFFFFF;
     text-align: center;
     font-size: 16px;
     border-radius: 12px;
     color: black;
-    //padding: 16px;
     box-shadow: 0px 4px 4px rgba(0, 0, 0, 0.25);
     align-content: center;
     align-items: center;
-    //width:100%;
     padding-left:20px;
     padding-right:40px;
 `
 const Buttonscontainer = styled(EmptyBox)`
     flex-direction:row;
-    //display:flex;
-    //margin: 10px;
     justify-content: space-evenly;
     flex-grow: 0;
 `
@@ -288,17 +229,6 @@ const ImgG = styled.img`
     height:34px;
     width:34px;
 `
-// const EmptyBox2 = styled.div`
-//     //min-height:50px;
-//     //min-width:417px;
-//     margin: 10px;
-//     flex-direction: row;
-//     text-align:center;
-//     font-weight: bold;
-//     font-size: 14px;
-//     align-content:center;
-//     align-items:center;
-// `
 const MyLink = styled(Link)`
 color: ${props=>props.active?"#738F9C":"black"} ;
 text-decoration: none; 
@@ -313,22 +243,15 @@ text-align:center;
 align-items: center;
 margin: 10px;
 justify-content:center;
-//width:100%;
 `
 const MyLink2 = styled(Link)`
 text-decoration: none;
 color:#738F9C; 
 display: flex;
 flex-direction: row;
-//align-content: center;
-//padding-left: 15px;
 &:hover{color: #9C9083};
 font-weight: medium;
 font-size: 14px;
-//text-align:center;
-//align-items: center;
-//margin: 10px;
 justify-content:flex-end;
-//width:100%;
 margin-right:12px;
 `
