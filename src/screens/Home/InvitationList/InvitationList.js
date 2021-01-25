ae2c76e03bd9652eae65cf8c361fade8738f2a4e
Kamil <kamilpier08@gmail.com>
2021-01-25 12:42:01 +0100

    Code refactor, notifications settings

--- src/screens/Home/InvitationList/InvitationList.js
+++ src/screens/Home/InvitationList/InvitationList.js
@@ -1,97 +1,99 @@
-import React, {useEffect, useState} from 'react'
-import styled from 'styled-components'
-import {InvitationCard} from './InvitationCard'
-import firebase from 'firebase'
+import React, { useEffect, useState } from 'react';
+import styled from 'styled-components';
+import { InvitationCard } from './InvitationCard';
+import firebase from 'firebase';
 
 export function InvitationList() {
-  const [invitationList, setList] = useState(new Array());
+	const [ invitationList, setList ] = useState(new Array());
 
-  useEffect(async () => {
-    if(firebase.auth().currentUser !== null) {
-      // TODO please refactor this piece of code
-      const invitations = firebase.firestore().collection('Users').doc(firebase.auth().currentUser.uid).collection("GroupsInvitations");
-      const invitationsSubscription = invitations.onSnapshot(snapshot => {
-        renderList(snapshot)
-      })
-      return () => {
-        invitationsSubscription()
-      }
-    }
-  }, [])
+	useEffect(async () => {
+		if (firebase.auth().currentUser !== null) {
+      const invitations = firebase.
+                           firestore().
+                           collection('Users').
+                           doc(firebase.auth().currentUser.uid).
+                           collection('GroupsInvitations');
+			const invitationsSubscription = invitations.onSnapshot((snapshot) => {
+				renderList(snapshot);
+			});
+			return () => {
+				invitationsSubscription();
+			};
+		}
+	}, []);
 
-  const renderList = (snapshot) => {
-    let array = new Array()
-    snapshot.docs.map(doc => {
-      let data = {
-        id: doc.id,
-        msg: doc.data().msg
-      }
-      array.push(
-        <InvitationCard value={data}/>
-      )
-    })
-    console.log(array)
-    setList(array)
-  }
+	const renderList = (snapshot) => {
+		let array = new Array();
+		snapshot.docs.map((doc) => {
+			let data = {
+				id  : doc.id,
+				msg : doc.data().msg
+			};
+			array.push(<InvitationCard value={data} />);
+		});
+		console.log(array);
+		setList(array);
+	};
 
-  return (
-    <Container>
-      <Header>
-        <HeaderText>AKTYWNE ZAPROSZENIA</HeaderText>
-      </Header>
-      <List>
-        {invitationList}
-      </List>
-    </Container>
-  )
+	return (
+		<Container>
+			<Header>
+				<HeaderText>AKTYWNE ZAPROSZENIA</HeaderText>
+			</Header>
+			<List>{invitationList}</List>
+		</Container>
+	);
 }
 
-
 const Container = styled.div`
-  //grid-area: span 1;
-  //grid-row: span 4;
-  min-height: 400px;
-  border: #73909C solid 2px;
-  border-radius: 12px;
-  box-sizing: border-box;
-  position: relative;
-`
+	//grid-area: span 1;
+	//grid-row: span 4;
+	min-height: 400px;
+	border: #73909c solid 2px;
+	border-radius: 12px;
+	box-sizing: border-box;
+	position: relative;
+`;
 
 const Header = styled.div`
-  position: absolute;
-  top: 0; right: 0;
-  padding: 10px 25px 12px 25px;
-  background-color: #73909C;
-  border-radius: 0 9px 0 12px;
-  box-sizing: border-box;
-`
+	position: absolute;
+	top: 0;
+	right: 0;
+	padding: 10px 25px 12px 25px;
+	background-color: #73909c;
+	border-radius: 0 9px 0 12px;
+	box-sizing: border-box;
+`;
 
 const HeaderText = styled.p`
-  color: rgba(255,255,255,.83);
-  padding: 0;
-  margin: 0;
-  font-weight: 500;
-  font-size: 16;
-`
+	color: rgba(255, 255, 255, .83);
+	padding: 0;
+	margin: 0;
+	font-weight: 500;
+	font-size: 16;
+`;
 
 const List = styled.div`
-  position: absolute;
-  top: 45px; left: 0; right: 0; bottom: 0;
-  padding: 0px 15px 0 15px;
-  display: flex;
-  flex-direction: column;
-  align-content: stretch;
-  overflow-y: auto;
+	position: absolute;
+	top: 45px;
+	left: 0;
+	right: 0;
+	bottom: 0;
+	padding: 0px 15px 0 15px;
+	display: flex;
+	flex-direction: column;
+	align-content: stretch;
+	overflow-y: auto;
 
-  &::-webkit-scrollbar {
-    width: 10px;
-  }
+	&::-webkit-scrollbar {
+		width: 10px;
+	}
 
-  &::-webkit-scrollbar-track {
-    box-shadow: inset 0 0 0px #9C9083;
-  }
-  &::-webkit-scrollbar-thumb {
-    background-color: #9C9083;
-    border-radius: 5px;
-  }
-`
+	&::-webkit-scrollbar-track {
+		box-shadow: inset 0 0 0px #9c9083;
+	}
+	&::-webkit-scrollbar-thumb {
+		background-color: #9c9083;
+		border-radius: 5px;
+	}
+`;
